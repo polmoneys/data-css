a799d4b2820716e30a6cade3d9e0782c206d8d69
Pol Moneys <p0lm0n3ys@gmail.com>
2023-11-04 18:24:20 +0100

    Feat - Adds more control to you when building /package

---
+++ package/concat-css.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+
+const sourceFolders = [
+    './src/theme',
+    './src/fonts',
+    './src/form',
+    './src/group',
+    './src/media',
+    './src/utils',
+];
+const destinationFile = './dist/merged.css';
+
+const mergedContent = sourceFolders.reduce((content, folder) => {
+    const files = fs.readdirSync(folder);
+
+    files.forEach((file) => {
+        if (file.endsWith('.css')) {
+            const filePath = path.join(folder, file);
+            const fileContent = fs.readFileSync(filePath, 'utf-8');
+            content += fileContent + '\n';
+        }
+    });
+
+    return content;
+}, '');
+
+fs.writeFileSync(destinationFile, mergedContent, 'utf-8');
+
+console.log('CSS files concatenated successfully.');
